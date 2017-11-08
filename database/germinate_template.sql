922f9fd521e0346e09a7e556f6528b3993e4bba4
Sebastian Raubach <sebastian-raubach@users.noreply.github.com>
2017-11-08 10:31:54 +0000

    - FIX: Fixed wrong checksum
    - FIX: Fixed wrong checksum in data migration table.

--- database/germinate_template.sql
+++ database/germinate_template.sql
@@ -1385,7 +1385,7 @@ INSERT INTO `schema_version` VALUES (2, '3.3.2', 'update', 'SQL', 'V3.3.2__updat
 INSERT INTO `schema_version` VALUES (3, '3.3.2.1', 'update', 'SQL', 'V3.3.2.1__update.sql', -256506759, 'germinate3', '2016-11-03 15:46:40', 123, 1);
 INSERT INTO `schema_version` VALUES (4, '3.3.2.2', 'update', 'SQL', 'V3.3.2.2__update.sql', 508407614, 'germinate3', '2016-11-04 10:31:18', 9, 1);
 INSERT INTO `schema_version` VALUES (5, '3.4.0', 'update', 'SQL', 'V3.4.0__update.sql', 1635546146, 'germinate3', '2017-01-10 14:23:11', 198, 1);
-INSERT INTO `schema_version` VALUES (6, '3.4.0.1', 'update', 'SQL', 'V3.4.0.1__update.sql', 278320740, 'germinate3', '2017-09-28 15:58:00', 161, 1);
+INSERT INTO `schema_version` VALUES (6, '3.4.0.1', 'update', 'SQL', 'V3.4.0.1__update.sql', -1497522993, 'germinate3', '2017-09-28 15:58:00', 161, 1);
 
 -- ----------------------------
 -- Table structure for storage
